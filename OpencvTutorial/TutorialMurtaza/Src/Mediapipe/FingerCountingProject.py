97232157ad01a712e25445d7bc005378b7e8f7ee
palindungan <rizkika.palindungan1@gmail.com>
2021-06-21 01:17:24 +0700

    done

--- OpencvTutorial/TutorialMurtaza/Src/Mediapipe/FingerCountingProject.py
+++ OpencvTutorial/TutorialMurtaza/Src/Mediapipe/FingerCountingProject.py
@@ -2,6 +2,7 @@ import cv2
 import os
 import time
 from TutorialMurtaza.Util import BaseFunction
+import HandTrackingModule as htm
 
 #############
 wCam, hCam = 640, 480
@@ -25,12 +26,47 @@ print(len(overlayList))
 
 pTime = 0
 
+detector = htm.HandDetector(detectionCon=0.75)
+
+tipIds = [4, 8, 12, 16, 20]
+
 while True:
     success, img = cap.read()
 
-    h, w, c = overlayList[0].shape
-    # img[0:y, 0:x] img[height, width] image size is 200
-    img[0:h, 0:w] = overlayList[0]
+    img = detector.findHands(img)
+    lmList = detector.findPosition(img, draw=False)
+    # print(lmList)
+
+    if len(lmList) != 0:
+        fingers = []
+
+        # thumb
+        if lmList[tipIds[0]][1] > lmList[tipIds[0] - 1][1]:
+            # print('Index Finger Open')
+            fingers.append(1)
+        else:
+            fingers.append(0)
+
+        # 4 other fingers
+        for id in range(1, 5):
+            if lmList[tipIds[id]][2] < lmList[tipIds[id] - 2][2]:
+                # print('Index Finger Open')
+                fingers.append(1)
+            else:
+                fingers.append(0)
+
+        # print(fingers)
+        totalFingers = fingers.count(1)
+        print(totalFingers)
+
+        h, w, c = overlayList[totalFingers - 1].shape
+        # img[0:y, 0:x] img[height, width] image size is 200
+        img[0:h, 0:w] = overlayList[totalFingers - 1]
+        # in python if we choose -1 in list then in will choose last item of the list
+
+        # show rectangle and text
+        cv2.rectangle(img, (20, 225), (170, 425), (0, 225, 0), cv2.FILLED)
+        cv2.putText(img, str(totalFingers), (45, 375), cv2.FONT_HERSHEY_PLAIN, 10, (255, 0, 0), 25)
 
     cTime = time.time()
     fps = 1 / (cTime - pTime)
