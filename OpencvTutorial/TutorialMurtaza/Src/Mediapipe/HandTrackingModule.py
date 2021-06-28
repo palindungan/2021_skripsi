51721f4329932edafae20495a3e98999b525f02d
palindungan <rizkika.palindungan1@gmail.com>
2021-06-29 01:49:04 +0700

    line info

--- OpencvTutorial/TutorialMurtaza/Src/Mediapipe/HandTrackingModule.py
+++ OpencvTutorial/TutorialMurtaza/Src/Mediapipe/HandTrackingModule.py
@@ -1,6 +1,7 @@
 import cv2
 import mediapipe as mp
 import time
+import math
 
 
 class HandDetector():
@@ -94,6 +95,21 @@ class HandDetector():
                 fingers.append(0)
 
         return fingers
+
+    def findDistance(self, p1, p2, img, draw=True):
+        x1, y1 = self.lmList[p1][1], self.lmList[p1][2]
+        x2, y2 = self.lmList[p2][1], self.lmList[p2][2]
+        cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
+
+        if draw:
+            cv2.circle(img, (x1, y1), 15, (255, 0, 0), cv2.FILLED)
+            cv2.circle(img, (x2, y2), 15, (255, 0, 0), cv2.FILLED)
+            cv2.line(img, (x1, y1), (x2, y2), (255, 0, 0), 3)
+            cv2.circle(img, (cx, cy), 15, (255, 0, 0), cv2.FILLED)
+
+        length = math.hypot(x2 - x1, y2 - y1)
+
+        return length, img, [x1, y1, x2, y2, cx, cy]
 
 
 def main():
--- OpencvTutorial/TutorialMurtaza/Src/Mediapipe/VolumeHandControlAdvance.py
+++ OpencvTutorial/TutorialMurtaza/Src/Mediapipe/VolumeHandControlAdvance.py
@@ -55,24 +55,13 @@ while True:
         if 250 < area < 1000:
             print('yes')
             # Find Distance between index and Thumb
+            length, img, lineInfo = detector.findDistance(4, 8, img)
             # Convert Volume
             # Reduce Resolution to make it smoother / not change when detect very small distance
             # Check Finger Up
             # If pinky is down set volume
             # Drawings
             # Frame Rate
-
-            x1, y1 = lmList[4][1], lmList[4][2]
-            x2, y2 = lmList[8][1], lmList[8][2]
-            cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
-
-            cv2.circle(img, (x1, y1), 15, (255, 0, 0), cv2.FILLED)
-            cv2.circle(img, (x2, y2), 15, (255, 0, 0), cv2.FILLED)
-            cv2.line(img, (x1, y1), (x2, y2), (255, 0, 0), 3)
-            cv2.circle(img, (cx, cy), 15, (255, 0, 0), cv2.FILLED)
-
-            length = math.hypot(x2 - x1, y2 - y1)
-            # print(length)
 
             # pixel range = 50 - 300
             # volume range = -65 - 0
@@ -83,7 +72,7 @@ while True:
             volume.SetMasterVolumeLevel(vol, None)
 
             if length < 50:
-                cv2.circle(img, (cx, cy), 15, (0, 255, 0), cv2.FILLED)
+                cv2.circle(img, (lineInfo[4], lineInfo[5]), 15, (0, 255, 0), cv2.FILLED)
 
     cv2.rectangle(img, (50, 150), (85, 400), (0, 255, 0), 3)
     cv2.rectangle(img, (50, int(volumeBar)), (85, 400), (0, 255, 0), cv2.FILLED)
